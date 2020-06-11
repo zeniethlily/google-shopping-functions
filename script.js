70433687ef15d8e01de098af0ab18c51d654c294
Darrell Yeo <zeniethlily@gmail.com>
2020-06-11 21:23:20 +0800

    basic functions defined and tested working

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
-console.log("hello script js");
+
+function getItems(products) {
+  return products.items;
+}
+var list = [];
+function getItemsByBrand(items, brand) {
+  for(let i = 0; i < items.length; i++) {
+    if(items[i].product.brand == brand) {
+      list.push(items[i]);
+      console.log("found a " + brand);
+    }
+    //console.log(products.items[i]);
+  }
+  return list;
+}
+
+
+function getItemsByAuthor(items, author){
+  for(let i = 0; i < items.length; i++) {
+    if(items[i].product.author.name == author) {
+      list.push(items[i]);
+      console.log("found a " + author);
+    }
+    //console.log(products.items[i]);
+  }
+  return list;
+}
+
+function getAvailableProducts(items){
+  for(let i = 0; i < items.length; i++) {
+    if(items[i].product.inventories[0].availability == "inStock") {
+      list.push(items[i]);
+      console.log("item is in stock");
+    }
+    //console.log(products.items[i]);
+  }
+  return list;
+}
+ 
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
+  var allItemArr = getItems( products );
+  //ar result = getItemsByBrand(allItemArr, 'Nikon');
+  //var result = getItemsByAuthor(allItemArr, 'Target');
+  var result = getAvailableProducts(allItemArr);
+  console.log(result);
+
   return "WOW GOOGLE SHOPPING";
 };
